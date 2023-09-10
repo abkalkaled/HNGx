aa3831f2008cb0a614ec5cf86ec35cdce5e23d17
Aminu Khaleed <aminukhaleedabiola@gmail.com>
2023-09-10 21:32:20 +0100

    Date and Time Fixed

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function dispayDay() {
+  const daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const dayIndex = currentDate.getUTCDay();
+  const dayOfTheWeek = daysOfTheWeek[dayIndex];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    "Day of the week" + dayOfTheWeek;
+}
+
 function displayUTCTime() {
   const now = new Date();
   const utcString = now.toUTCString();
